7efc46e3bb62e02a096129e62e9c3102cec23d5b
Yahia Ait Bouachrine <yahia322@gmail.com>
2023-03-18 03:39:15 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
-
 /**
  * main - entry point
  * Return: always 0 (success)
 */
-
 int main(void)
 {
 	int digit = 0;
 
 	while (digit <= 9)
 	{
-	
 		putchar(digit + '0');
 		++digit;
 	}
 	putchar('\n');
-
 	return (0);
 }
